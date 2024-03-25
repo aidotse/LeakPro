cd8eb882c9c399f335daef8c9a66a2f702db722b
johanos1 <oshtman@gmail.com>
2024-03-25 21:58:55 +0000

    store the shadow models in folder to be reused

--- leakpro/models.py
+++ leakpro/models.py
@@ -63,6 +63,26 @@ class ConvNet(nn.Module):
         return self.fc3(x)
 
 
+class SmallerSingleLayerConvNet(nn.Module):
+    """Smaller Convolutional Neural Network model with only one convolutional layer."""
 
+    def __init__(self:Self) -> None:
+        """Initialize the SmallerSingleLayerConvNet model."""
+        super().__init__()
+        # Only one convolutional layer
+        self.conv1 = nn.Conv2d(3, 4, 5)
+        self.pool = nn.MaxPool2d(2, 2)
 
+        # Adjusting the linear layers since we now have only one conv layer
+        # Assuming the input image size is 32x32, after one convolution and pooling, the size is reduced to 14x14 (32 -> 28 -> 14)
+        self.fc1 = nn.Linear(4 * 14 * 14, 120)  # Adjusted to match the output of the pooling layer
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
+    def forward(self:Self, x:torch.Tensor) -> torch.Tensor:
+        """Forward pass of the model."""
+        x = self.pool(F.relu(self.conv1(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except the batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
