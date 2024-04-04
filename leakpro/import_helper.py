3dcd1cfcffb741d406afaf7d61c76c81f4994a03
johanos1 <oshtman@gmail.com>
2024-04-04 08:25:47 +0000

    forgot importhelper.py

--- leakpro/import_helper.py
+++ leakpro/import_helper.py
@@ -5,4 +5,8 @@ from typing import Any, Callable, Dict, List, Tuple, TypeVar, Union  # noqa: F40
 if sys.version_info >= (3, 11):
     from typing import Self
 else:
+<<<<<<< HEAD
     Self = TypeVar("Self")
+=======
+    Self = TypeVar("Self")
+>>>>>>> 2db0b25 (forgot importhelper.py)
