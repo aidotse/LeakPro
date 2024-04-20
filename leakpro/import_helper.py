ae29f02e96fc3893c6d44174a0a947dcb3a97f1d
johanos1 <oshtman@gmail.com>
2024-04-20 21:39:58 +0000

    shadow model training update

--- leakpro/import_helper.py
+++ leakpro/import_helper.py
@@ -1,6 +1,6 @@
 """Compatibility module for various Python versions."""
 import sys
-from types import ModuleType
+from types import ModuleType  # noqa: F401
 from typing import Any, Callable, Dict, List, Tuple, TypeVar, Union  # noqa: F401
 
 if sys.version_info >= (3, 11):
