e87d9b157f5d58f59b804ae6bfa90e3f9038fca1
johanos1 <oshtman@gmail.com>
2024-08-27 13:19:22 +0000

    fixture to create and tear down tmp folder

--- leakpro/tests/conftest.py
+++ leakpro/tests/conftest.py
@@ -1,6 +1,7 @@
 """Test for the image input handler."""
 import logging
 import os
+import shutil
 from typing import Generator
 
 import pytest
@@ -10,6 +11,21 @@ from leakpro.tests.input_handler.image_utils import setup_image_test
 from leakpro.user_inputs.cifar10_input_handler import Cifar10InputHandler
 from leakpro.attacks.utils.shadow_model_handler import ShadowModelHandler
 from leakpro.tests.constants import STORAGE_PATH
+
+
+@pytest.fixture(scope="session")
+def manage_storage_directory():
+    """Fixture to create and remove the storage directory."""
+    
+    # Setup: Create the folder at the start of the test session
+    os.makedirs(STORAGE_PATH, exist_ok=True)
+    
+    # Yield control back to the test session
+    yield
+    
+    # Teardown: Remove the folder and its contents at the end of the session
+    if os.path.exists(STORAGE_PATH):
+        shutil.rmtree(STORAGE_PATH)
 
 @pytest.fixture()
 def image_handler() -> Generator[Cifar10InputHandler, None, None]:
@@ -24,15 +40,8 @@ def image_handler() -> Generator[Cifar10InputHandler, None, None]:
 
     handler = Cifar10InputHandler(config, logger)
 
+    # Yield control back to the test session
     yield handler
-
-    # Clean up folder when tests are done
-    if os.path.exists(config.target.data_path):
-        os.remove(config.target.data_path) # remove data
-    if os.path.exists(config.target.module_path):
-        os.remove(config.target.module_path)
-    if os.path.exists(config.target.trained_model_path):
-        os.remove(config.target.trained_model_metadata_path) # remove target metadata
 
 @pytest.fixture()
 def offline_shadow_model_handler(image_handler) -> Generator[ShadowModelHandler, None, None]:
@@ -53,8 +62,5 @@ def offline_shadow_model_handler(image_handler) -> Generator[ShadowModelHandler,
     shadow_handler = ShadowModelHandler(image_handler)
     shadow_handler.create_shadow_models(n_models, image_handler.test_indices, training_fraction, online)
     
+    # Yield control back to the test session
     yield shadow_handler
-
-    # Clean up folder tests are done
-    if os.path.exists(STORAGE_PATH + "/shadow_models"):
-        os.remove(STORAGE_PATH + "/shadow_models")
