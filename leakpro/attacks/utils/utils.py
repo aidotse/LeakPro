c6b8b19e8e2495978250eb059752c45ad1b5ce75
johanos1 <oshtman@gmail.com>
2024-08-29 14:26:16 +0000

    updated based on review comments

--- leakpro/attacks/utils/utils.py
+++ leakpro/attacks/utils/utils.py
@@ -4,18 +4,19 @@ import numpy as np
 from torch import exp, from_numpy, max, sum
 
 
-def softmax_logits(logits: np.ndarray, temp:float=2.0) -> np.ndarray:
+def softmax_logits(logits: np.ndarray, temp:float=1.0, dimension:int=-1) -> np.ndarray:
     """Rescale logits to (0, 1).
 
     Args:
     ----
         logits ( len(dataset) x ... x nb_classes ): Logits to be rescaled.
         temp (float): Temperature for softmax.
+        dimension (int): Dimension to apply softmax.
 
     """
     logits = from_numpy(logits) / temp
-    logits = logits - max(logits, dim=-1, keepdim=True).values
+    logits = logits - max(logits, dim=dimension, keepdim=True).values
     logits = exp(logits)
-    logits = logits/sum(logits, dim=-1, keepdim=True)
+    logits = logits/sum(logits, dim=dimension, keepdim=True)
     return logits.numpy()
 
