d57b703a8ec91be472caec3d3f0e68d182b5ed80
fazelehh <78154933+fazelehh@users.noreply.github.com>
2025-01-10 12:43:36 +0100

    GRU model for LOS (#170)
    * ReadMe file for creating dataset from MIMICIII, and training LOS>3 classifier
    
    * adding links to readMe file
    
    * fixing the LR model
    
    * Mimic_Hnadler, data_processeing, model and the main file
    
    * all attacks are running
    
    * add relevant files from MIMIC_Extract and mimic_code repos
    
    * fix some bugs regarding nivduration
    
    * revert conflicting tabular_mia files
    
    * adding subset to the mimicDataset
    
    * fixing loading the database and indices
    
    * adding GRU
    
    * adding GRU-D structure
    
    * updating grud class
    
    * adding early stopping
    
    * the model is running
    
    * the model is running
    
    * working on meta data
    
    * mimic hanlder gru is added
    
    * LiRA in place
    
    * running lira
    
    * LiRA and RMIA for GRU, temp implmemntation
    
    * adding flag for changing the classifier
    
    * LR and read me
    
    * up to adding report handler module
    
    * renaming files, adding report handler module
    
    * renaming files, adding report handler module
    
    * adding the license, renaming some files and classes
    
    * fixing the accuracy in model handler
    
    * fixing pandas warning and adding a check to saved shadow models model type
    
    * fixing a bug in shadow_model_handler
    
    * ruff fix

--- examples/mia/LOS/utils/model_LR.py
+++ examples/mia/LOS/utils/model_LR.py
@@ -1,19 +1,20 @@
+import os
+import pickle
 
 import tqdm as tqdm
-import pickle
-import torch.nn as nn
-from torch import device, optim, no_grad, save, sigmoid, cuda
+from torch import cuda, device, nn, no_grad, optim, save, sigmoid
 
 
-class MimicLR(nn.Module):
+class LR(nn.Module):
     def __init__(self, input_dim: int):
         """Initialize the logistic regression model with a single linear layer.
 
         Args:
         ----
             input_dim (int): The size of the input feature vector.
+
         """
-        super(MimicLR, self).__init__()
+        super(LR, self).__init__()
         self.linear = nn.Linear(input_dim, 1)  # Binary classification (1 output)
         # Metadata initialization
         self.init_params = {"input_dim": input_dim}
@@ -24,26 +25,24 @@ class MimicLR(nn.Module):
 
 def evaluate(model, loader, criterion, device):
     model.eval()
-    loss, correct = 0, 0
-    for data, target in loader:
-        data, target = data.to(device), target.to(device)
-        target = target.float().unsqueeze(1)
-        
-        with no_grad():
+    loss, acc = 0, 0
+    with no_grad():
+        for data, target in loader:
+            data, target = data.to(device), target.to(device)
+            target = target.float().unsqueeze(1)
             output = model(data)
             loss += criterion(output, target).item()
-            pred = output >= 0.5
-            correct += (pred == target).float().sum()
-    acc = float(correct)/ len(loader.dataset)
-    loss /= len(loader)
-    
+            pred = (output) >= 0.5
+            acc += pred.eq(target.data.view_as(pred)).sum()
+        loss /= len(loader)
+        acc = float(acc) / len(loader.dataset)
     return loss, acc
 
 
 def create_trained_model_and_metadata(model,
                                       train_loader,
                                       test_loader,
-                                      epochs , 
+                                      epochs ,
                                       lr ,
                                       weight_decay ,
                                       metadata = None):
@@ -52,17 +51,17 @@ def create_trained_model_and_metadata(model,
     model.to(device_name)
     model.train()
 
-    criterion = nn.BCELoss(reduction="mean")
+    criterion = nn.BCELoss()
     optimizer = optim.SGD(model.parameters(),
                           lr = lr,
                           weight_decay = weight_decay)
     train_losses, train_accuracies = [], []
     test_losses, test_accuracies = [], []
-    
+
     for e in tqdm.tqdm(range(epochs), desc="Training Progress"):
         model.train()
         train_acc, train_loss = 0.0, 0.0
-        
+
         for data, target in train_loader:
             target = target.float().unsqueeze(1)
             data, target = data.to(device_name, non_blocking=True), target.to(device_name, non_blocking=True)
@@ -70,27 +69,30 @@ def create_trained_model_and_metadata(model,
             output = model(data)
 
             loss = criterion(output, target)
-            pred =output >= 0.5
+            pred = (output) >= 0.5
             train_acc += pred.eq(target).sum().item()
-            
+
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
-        
-        epoch_train_loss = train_loss / len(train_loader)
-        epoch_train_acc = train_acc / len(train_loader.dataset)
-            
-        train_losses.append(epoch_train_loss)
-        train_accuracies.append(epoch_train_acc)
-        
+
+        train_loss /= len(train_loader)
+        train_acc /= len(train_loader.dataset)
+
+        train_losses.append(train_loss)
+        train_accuracies.append(train_acc)
+
         test_loss, test_acc = evaluate(model, test_loader, criterion, device_name)
-        # _ , train_loss = evaluate(model, train_loader, criterion, device_name)
         test_losses.append(test_loss)
         test_accuracies.append(test_acc)
-    
+
     # Move the model back to the CPU
     model.to("cpu")
-    with open("target/target_model.pkl", "wb") as f:
+
+
+    if not os.path.exists("target_LR"):
+        os.makedirs("target_LR")
+    with open("target_LR/target_model.pkl", "wb") as f:
         save(model.state_dict(), f)
 
     # Create metadata and store it
@@ -98,12 +100,12 @@ def create_trained_model_and_metadata(model,
     meta_data["train_indices"] = train_loader.dataset.indices
     meta_data["test_indices"] = test_loader.dataset.indices
     meta_data["num_train"] = len(meta_data["train_indices"])
-    
+
     # Write init params
     meta_data["init_params"] = {}
     for key, value in model.init_params.items():
         meta_data["init_params"][key] = value
-    
+
     # read out optimizer parameters
     meta_data["optimizer"] = {}
     meta_data["optimizer"]["name"] = optimizer.__class__.__name__.lower()
@@ -124,8 +126,9 @@ def create_trained_model_and_metadata(model,
     meta_data["train_loss"] = train_loss
     meta_data["test_loss"] = test_loss
     meta_data["dataset"] = "mimiciii"
-    
-    with open("target/model_metadata.pkl", "wb") as f:
+
+
+    with open("target_LR/model_metadata.pkl", "wb") as f:
         pickle.dump(meta_data, f)
-    
+
     return train_accuracies, train_losses, test_accuracies, test_losses
