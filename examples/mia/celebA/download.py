4ff0ab9a0b0298365ed837e783fe3cf9f25697ec
fazelehh <fazeleh.hoseini@ai.se>
2025-01-13 21:19:15 +0000

    remove celebA related files

--- examples/mia/celebA/download.py
+++
@@ -1,8 +0,0 @@
-from torchvision.transforms import ToTensor, Normalize, Compose
-from torchvision.datasets import CelebA
-root =  "./data"
-transform = Compose([
-                ToTensor(),
-                Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalization for CelebA
-            ])
-celebA_dataset = CelebA(root=root, split="all", target_type= "identity" , download=True,  transform=transform)
