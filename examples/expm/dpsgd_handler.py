2b4d386be34e52b31e03681943142aee508b9a8d
fazelehh <fazeleh.hoseini@ai.se>
2025-02-19 09:41:11 +0000

    removing dpsgd from shadow models

--- examples/expm/dpsgd_handler.py
+++ examples/expm/dpsgd_handler.py
@@ -45,48 +45,48 @@ class MimicInputHandlerGRU(AbstractInputHandler):
     ) -> dict:
 
 
-        print("Training shadow models with DP-SGD")
-        dpsgd_path = self.configs["audit"]["dpsgd"]["dpsgd_path"]
+        # print("Training shadow models with DP-SGD")
+        # dpsgd_path = self.configs["audit"]["dpsgd"]["dpsgd_path"]
 
-        sample_rate = 1/len(dataloader)
-        # Check if the file exists
-        if os.path.exists(dpsgd_path):
-            # Open and read the pickle file
-            with open(dpsgd_path, "rb") as file:
-                privacy_engine_dict = pickle.load(file)
-            print("Pickle file loaded successfully!")
-            print("Data:", privacy_engine_dict)
-        else:
-            raise Exception(f"File not found at: {dpsgd_path}")
+        # sample_rate = 1/len(dataloader)
+        # # Check if the file exists
+        # if os.path.exists(dpsgd_path):
+        #     # Open and read the pickle file
+        #     with open(dpsgd_path, "rb") as file:
+        #         privacy_engine_dict = pickle.load(file)
+        #     print("Pickle file loaded successfully!")
+        #     print("Data:", privacy_engine_dict)
+        # else:
+        #     raise Exception(f"File not found at: {dpsgd_path}")
 
-        try:
-            noise_multiplier = get_noise_multiplier(target_epsilon = privacy_engine_dict["target_epsilon"],
-                                            target_delta = privacy_engine_dict["target_delta"],
-                                            sample_rate = sample_rate ,
-                                            epochs = privacy_engine_dict["epochs"],
-                                            epsilon_tolerance = privacy_engine_dict["epsilon_tolerance"],
-                                            accountant = "prv",
-                                            eps_error = privacy_engine_dict["eps_error"],)
-        except:
-            # the prv accountant is not robust to large epsilon (even epsilon = 10)
-            # so we will use rdp when it fails, so the actual epsilon may be slightly off
-            # see https://github.com/pytorch/opacus/issues/604
-            noise_multiplier = get_noise_multiplier(target_epsilon = 2,
-                                                    target_delta = privacy_engine_dict["target_delta"],
-                                                    sample_rate = sample_rate,
-                                                    epochs = privacy_engine_dict["epochs"],
-                                                    epsilon_tolerance = privacy_engine_dict["epsilon_tolerance"],
-                                                    accountant = "rdp")
+        # try:
+        #     noise_multiplier = get_noise_multiplier(target_epsilon = privacy_engine_dict["target_epsilon"],
+        #                                     target_delta = privacy_engine_dict["target_delta"],
+        #                                     sample_rate = sample_rate ,
+        #                                     epochs = privacy_engine_dict["epochs"],
+        #                                     epsilon_tolerance = privacy_engine_dict["epsilon_tolerance"],
+        #                                     accountant = "prv",
+        #                                     eps_error = privacy_engine_dict["eps_error"],)
+        # except:
+        #     # the prv accountant is not robust to large epsilon (even epsilon = 10)
+        #     # so we will use rdp when it fails, so the actual epsilon may be slightly off
+        #     # see https://github.com/pytorch/opacus/issues/604
+        #     noise_multiplier = get_noise_multiplier(target_epsilon = 2,
+        #                                             target_delta = privacy_engine_dict["target_delta"],
+        #                                             sample_rate = sample_rate,
+        #                                             epochs = privacy_engine_dict["epochs"],
+        #                                             epsilon_tolerance = privacy_engine_dict["epsilon_tolerance"],
+        #                                             accountant = "rdp")
 
         # make the model private
-        privacy_engine = PrivacyEngine(accountant = "prv")
-        model, optimizer, dataloader = privacy_engine.make_private(
-            module=model,
-            optimizer=optimizer,
-            data_loader=dataloader,
-            noise_multiplier=noise_multiplier,
-            max_grad_norm= privacy_engine_dict["max_grad_norm"],
-        )
+        # privacy_engine = PrivacyEngine(accountant = "prv")
+        # model, optimizer, dataloader = privacy_engine.make_private(
+        #     module=model,
+        #     optimizer=optimizer,
+        #     data_loader=dataloader,
+        #     noise_multiplier=noise_multiplier,
+        #     max_grad_norm= privacy_engine_dict["max_grad_norm"],
+        # )
 
         device_name = device("cuda" if cuda.is_available() else "cpu")
         model.to(device_name)
