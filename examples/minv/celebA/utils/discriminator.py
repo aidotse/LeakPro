fe1f702a5cacac0af81d0082fec5a2a2fd6d43c5
Hevaroo <hevar@live.se>
2025-02-24 09:16:16 +0000

    fix: remove unused discriminator class

--- examples/minv/celebA/utils/discriminator.py
+++ examples/minv/celebA/utils/discriminator.py
@@ -51,52 +51,6 @@ class SNResNetProjectionDiscriminator(nn.Module):
         return output
 
 
-class SNResNetConcatDiscriminator(nn.Module):
-
-    def __init__(self, num_features, num_classes, activation=F.relu,
-                 dim_emb=128):
-        super(SNResNetConcatDiscriminator, self).__init__()
-        self.num_features = num_features
-        self.num_classes = num_classes
-        self.dim_emb = dim_emb
-        self.activation = activation
-
-        self.block1 = OptimizedBlock(3, num_features)
-        self.block2 = Block(num_features, num_features * 2,
-                            activation=activation, downsample=True)
-        self.block3 = Block(num_features * 2, num_features * 4,
-                            activation=activation, downsample=True)
-        if num_classes > 0:
-            self.l_y = utils.spectral_norm(nn.Embedding(num_classes, dim_emb))
-        self.block4 = Block(num_features * 4 + dim_emb, num_features * 8,
-                            activation=activation, downsample=True)
-        self.block5 = Block(num_features * 8, num_features * 16,
-                            activation=activation, downsample=True)
-        self.l6 = utils.spectral_norm(nn.Linear(num_features * 16, 1))
-
-        self._initialize()
-
-    def _initialize(self):
-        init.xavier_uniform_(self.l6.weight.data)
-        if hasattr(self, 'l_y'):
-            init.xavier_uniform_(self.l_y.weight.data)
-
-    def forward(self, x, y=None):
-        h = x
-        h = self.block1(h)
-        h = self.block2(h)
-        h = self.block3(h)
-        if y is not None:
-            emb = self.l_y(y).unsqueeze(-1).unsqueeze(-1)
-            emb = emb.expand(emb.size(0), emb.size(1), h.size(2), h.size(3))
-            h = torch.cat((h, emb), dim=1)
-        h = self.block4(h)
-        h = self.block5(h)
-        h = torch.sum(self.activation(h), dim=(2, 3))
-        return self.l6(h)
-
-
-
 class Block(nn.Module):
 
     def __init__(self, in_ch, out_ch, h_ch=None, ksize=3, pad=1,
