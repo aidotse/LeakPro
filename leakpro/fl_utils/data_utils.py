5b9de1c0032e84cee688b87af7ccaf06fef5de47
mattiasakesson <ma10s@hotmail.com>
2025-03-24 14:44:13 +0100

    moves

--- leakpro/fl_utils/data_utils.py
+++ leakpro/fl_utils/data_utils.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 from torch import Tensor, cat, mean, randn, std, tensor
 from torch.utils.data import DataLoader, Dataset, TensorDataset
 import torch
+import numpy as np
 
 class GiaDataModalityExtension(ABC):
     @abstractmethod
@@ -25,6 +26,41 @@ class GiaImageClassifictaionExtension(GiaDataModalityExtension):
         reconstruction_dataset = TensorDataset(reconstruction, labels)
         reconstruction_loader = DataLoader(reconstruction_dataset, batch_size=32, shuffle=True)
         return reconstruction, labels, reconstruction_loader
+    
+class GiaNERExtension(GiaDataModalityExtension):
+    
+    def get_at_data(client_loader: DataLoader, token_used: np.ndarray=None) -> DataLoader:
+        """DataLoader with random noise images of the same shape as the client_loader's dataset, using the same labels."""
+        reconstruction_dataset = copy.deepcopy(client_loader.dataset)
+        reconstruction = []  # Collect embeddings to optimize
+        
+        for d in reconstruction_dataset:
+            
+            ind = np.where(np.array(d.labels)!=0)[0]#[0:1]
+            
+            token_used = torch.tensor(token_used, device=d.embedding.device)
+            ind = torch.tensor(ind, device=d.embedding.device)
+
+            d.embedding.index_put_((ind[:, None], token_used), torch.ones_like(d.embedding[ind[:, None],token_used])/54)#54)
+
+
+            #d.embedding[ind] = (d.embedding[ind].T/torch.sum(d.embedding[ind],1)).T
+            d.embedding.requires_grad = True 
+            
+            # make tokens with labels != 0 trainable
+            mask = torch.zeros_like(d.embedding)
+            mask[ind] = 1 
+            #d.embedding = PartialTrainableTensor.apply(d.embedding, mask).detach().requires_grad_(True)
+            def mask_grad(grad):
+                return grad * mask  # Apply the mask to the gradient
+            
+            d.embedding.register_hook(mask_grad)
+
+            # Add reference to the embedding for optimization
+            reconstruction.append(d.embedding)
+
+        reconstruction_loader = DataLoader(reconstruction_dataset, collate_fn=TrainingBatch, batch_size=1, shuffle=False)
+        return reconstruction, reconstruction_loader
 
 class CustomTensorDataset(Dataset):
     def __init__(self, reconstruction: torch.Tensor, labels: list):
