4cf4069dd05b716d1b78af3f3baccb5580f16819
Viktor Valadi <viktor.valadi@hotmail.se>
2025-06-30 13:31:31 +0000

    add optuna

--- examples/gia/pii_inverting_masked_text/main.py
+++ examples/gia/pii_inverting_masked_text/main.py
@@ -3,6 +3,7 @@ from data_.pii_data import get_pii_dataset
 from data_.data_extension import GiaNERExtension
 from leakpro.attacks.gia_attacks.invertinggradients_text import InvertingGradients, InvertingConfig
 from leakpro.run import run_gia_attack
+from leakpro.schemas import OptunaConfig
 from leakpro.utils.seed import seed_everything
 from train import train
 from transformers import LongformerTokenizerFast
@@ -28,20 +29,28 @@ if __name__ == "__main__":
     configs = InvertingConfig()
     configs.top10norms = False
     configs.median_pooling = False
-    configs.at_iterations = 10000
+    configs.at_iterations = 1000
     configs.tv_reg = 1.0e-06
     configs.attack_lr = 0.001
     configs.data_extension = GiaNERExtension()
-    configs.image_data = False
     
 
     # define dataloader
     client_dataset, data_mean, data_std = get_pii_dataset(num_docs=1)
     client_dataloader = torch.utils.data.DataLoader(client_dataset, collate_fn=TrainingBatch, batch_size=1, shuffle=False)
 
+    trial_data = []
+    for i in range(5):
+        trial_ds, _, _ = get_pii_dataset(num_docs=1, example_id=44+i)
+        trial_data.append(trial_ds)
+
     # baseline config
-    attack_object = InvertingGradients(model, client_dataloader, data_mean, data_std, configs=configs, train_fn=train)
-    run_gia_attack(attack_object, "TestInverting")
+    attack_object = InvertingGradients(model, client_dataloader, data_mean, data_std, configs=configs, train_fn=train, optuna_trial_data=trial_data)
+    optuna_config = OptunaConfig()
+    optuna_config.n_trials = 100
+    optuna_config.check_interval = 100
+    attack_object.run_with_optuna(optuna_config=optuna_config)
+
 
 
 
