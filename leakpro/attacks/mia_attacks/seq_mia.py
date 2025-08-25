7df2cfb02c4a00ba54590483d35f491045207207
Daniel Nilsson <daniel.nilsson@thep.lu.se>
2025-08-25 21:52:03 +0200

    Bug fix.

--- leakpro/attacks/mia_attacks/seq_mia.py
+++ leakpro/attacks/mia_attacks/seq_mia.py
@@ -277,8 +277,9 @@ class AttackSeqMIA(AbstractMIA):
                     p_target_i = p_target_i / torch.sum(p_target_i)
                     entropy = -torch.sum(p_target_i * torch.log(p_target_i))
                     p_except_target = torch.cat((p_target_i[:target_i],p_target_i[(target_i+1):]))
-                    mentropy = -(1 - p_target_i[target_i]) * torch.log(torch.max(p_target_i[target_i],1e-10)) - \
-                        torch.sum(p_except_target * torch.log(torch.max(1 - p_except_target),1e-10))
+                    mentropy = \
+                        -(1 - p_target_i[target_i]) * torch.log(torch.clamp(p_target_i[target_i],min=1e-10)) - \
+                        torch.sum(p_except_target * torch.log(torch.clamp(1 - p_except_target),min=1e-10))
                     loss_i = torch.stack([
                         criterion(logit_target_i, target_i),
                         torch.max(p_target_i),
@@ -305,8 +306,9 @@ class AttackSeqMIA(AbstractMIA):
                 p_target_i = p_target_i / torch.sum(p_target_i)
                 entropy = -torch.sum(p_target_i * torch.log(p_target_i))
                 p_except_target = torch.cat((p_target_i[:target_i],p_target_i[(target_i+1):]))
-                mentropy = -(1 - p_target_i[target_i]) * torch.log(torch.max(p_target_i[target_i],1e-10)) - \
-                    torch.sum(p_except_target * torch.log(torch.max(1 - p_except_target),1e-10))
+                mentropy = \
+                    -(1 - p_target_i[target_i]) * torch.log(torch.clamp(p_target_i[target_i],min=1e-10)) - \
+                    torch.sum(p_except_target * torch.log(torch.clamp(1 - p_except_target),min=1e-10))
                 loss_i = torch.stack([
                     criterion(logit_target_i, target_i),
                     torch.max(p_target_i),
