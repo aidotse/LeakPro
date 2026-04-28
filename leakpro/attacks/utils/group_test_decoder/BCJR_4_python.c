8f716ddb7e5d321ee015f681f9d18cd5a075116f
TheColdIce <edvin.callisen@gmail.com>
2026-04-28 20:58:59 +0000

    added headers with the Apache 2.0 boilerplate to every source file using licenseheaders pip package

--- leakpro/attacks/utils/group_test_decoder/BCJR_4_python.c
+++ leakpro/attacks/utils/group_test_decoder/BCJR_4_python.c
@@ -1,9 +1,17 @@
 /*
- * BCJR decoder for Group testing with
- * MAP decoding on Trellis
- * based on the work
- * "Optimum Detection of Defective Elements in Non-Adaptive Group Testing"
- * G. Liva, E. Paolini and M. Chiani, 2021.
+ * Copyright 2023-2026 AI Sweden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 /* C Libraries */
 
